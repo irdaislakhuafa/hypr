2dd7d896e03a8029bcffde7e108ba78c2161aad8
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-04-19 21:36:11 +0700

    add some autostart application

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-waybar &!
-hyprpaper &! 
-wifi off &!
-bluetooth off &! 
-brightnessctl -d 'intel_backlight' set 20% &!
+waybar &
+hyprpaper &
+wifi off &
+bluetooth off &
+brightnessctl -d 'intel_backlight' set 20% &
+pipewire &
+wireplumber &
+/usr/lib/xdg-desktop-portal-gtk &
+/usr/lib/xdg-desktop-portal-hyprland &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
