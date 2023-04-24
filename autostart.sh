0fbdcdf1f45e199f70dbbfcb30b069d398b4603f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-04-24 09:34:13 +0700

    use swaybg instead of hyprpaper

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
+
+# bar
 waybar &
-hyprpaper &
+
+# set wallpaper background
+#hyprpaper &
+swaybg -i $HOME/.config/hypr/assets/wallpaper.png
+
+# networks
 wifi off &
 bluetooth off &
+
+# screen brightness
 brightnessctl -d 'intel_backlight' set 20% &
-pipewire &
-wireplumber &
-/usr/lib/xdg-desktop-portal-gtk &
-/usr/lib/xdg-desktop-portal-hyprland &
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
+
+# for share screen
+#pipewire &
+#wireplumber &
+#/usr/lib/xdg-desktop-portal-gtk &
+#/usr/lib/xdg-desktop-portal-hyprland &
+#dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
