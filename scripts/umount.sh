713a57d0cbe737152983df40616afe0dc351a89d
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-05-26 20:00:27 +0700

    remove unused codes

--- scripts/umount.sh
+++ scripts/umount.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/sh
-alphabet=( {a..z} )
+alphabet=({a..z})
 part=""
-for ((a = 0 ; a <= 25; a++))
-do
-	for ((b = 0 ; b <= 128; b++))
-	do
+for ((a = 0; a <= 25; a++)); do
+	for ((b = 0; b <= 128; b++)); do
 		part=${alphabet[a]}
 		dev=$(ls /dev | grep sd$part$b)
 		current="sd$part$b"
 		if [ "$dev" == "$current" ]; then
-			udisksctl unmount -b /dev/$dev &!
-			
+			udisksctl unmount -b /dev/$dev
 		fi
 	done
-	udisksctl power-off -b /dev/sd$part &!
+	udisksctl power-off -b /dev/sd$part
 done
