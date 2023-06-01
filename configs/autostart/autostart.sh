f5d0e4d1dfcbe2cafd7245fb0a2c3022a57a135d
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-02 04:35:42 +0700

    added sutostart.conf

---
+++ configs/autostart/autostart.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# bar
+killall waybar
+waybar 2> /tmp/waybar &
+
+# set wallpaper background
+#hyprpaper &
+swaybg -i $HOME/.config/hypr/assets/wallpaper.png &
+
+# networks
+# wifi off &
+bluetooth off &
+
+# screen brightness
+brightnessctl -d 'intel_backlight' set 20% &
+
+# for share screen
+killall pipewire
+pipewire 2> /tmp/pipewire &
+
+killall wireplumber
+wireplumber 2> /tmp/wireplumber &
+
+killall xdg-desktop-portal-hyprland
+/usr/lib/xdg-desktop-portal-hyprland 2> /tmp/xdg-desktop-portal-hyprland &
+
