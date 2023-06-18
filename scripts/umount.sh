bbf8efe135e385262f6e8d1011dc1f035ba44686
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-18 08:46:04 +0700

    fix mount disk

--- scripts/umount.sh
+++ scripts/umount.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/sh
 alphabet=({a..z})
 part=""
+ignores=(
+	sda{1..10}
+)
 for ((a = 0; a <= 25; a++)); do
+	if [ "$(ls /dev | grep sd${alphabet[a]})" == '' ]; then
+		continue
+	fi
+
+	skip=0
 	for ((b = 0; b <= 128; b++)); do
 		part=${alphabet[a]}
 		dev=$(ls /dev | grep sd$part$b)
+		if [ "$dev" == "" ]; then
+			continue
+		fi
+
 		current="sd$part$b"
-		if [ "$dev" == "$current" ]; then
-			udisksctl unmount -b /dev/$dev
+		
+		if ! [ "$(echo ${ignores[@]} | grep '$dev')" == '' ]; then
+			if [ "$dev" == "$current" ]; then
+				udisksctl unmount -b /dev/$dev
+			fi
+		else 
+			skip=1
 		fi
 	done
-	udisksctl power-off -b /dev/sd$part
+	#if [ $skip -lt 1 ]; then
+		#udisksctl power-off -b /dev/sd$part
+	#fi
 done
