b3c361a9f66b8db0727594f7e6c96b7175d1c445
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-09-23 01:48:58 +0700

    save record screen location to /tmp/

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,5 @@
-RECORD_SCREEN_DESTINATION="$HOME/.cache/irdaislakhuafa_$(date '+%d_%B_%Y-%H.%M.%S').mp4"
+# RECORD_SCREEN_DESTINATION="$HOME/.cache/irdaislakhuafa_$(date '+%d_%B_%Y-%H.%M.%S').mp4"
+RECORD_SCREEN_DESTINATION="/tmp/$(date '+%d_%B_%Y-%H.%M.%S').mp4"
 NOTIFICATION_EXPIRE_TIME_IN_MILISEC=2000
 SCREEN_RECORDER_APP_NAME="WF Recorder"
 UNMOUNT_APP_NAME="umount.sh"
